a1e5df78a96c0f2e27bbaf4854906a019826b652
Joel Rafael Paredes Brioso <maria.brioso.joel@gmail.com>
2022-02-19 12:57:54 -0400

    Agregue el archivo .gitigonere y esta apuntando a produccion

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express"),
   app = express();
 require("dotenv").config();
 require("./connection");
+
 app.set("port", process.env.PORT || 3000);
 app.listen(app.get("port"), () => {
   console.log(`http://localhost:${app.get("port")}`);
